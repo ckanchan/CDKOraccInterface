4e8840c1e10f2bed97b95992e94c5e76e7647496
Chaitanya Kanchan <cdkanchan@gmail.com>
2019-06-14 17:19:50 +0100

    - Updated compatibility

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CDKOraccInterface",
-    platforms: [.macOS(.v10_12), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)],
+    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v2)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
