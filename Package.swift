e25c669bb487a0afc43e0d47148f363d3d211ebf
Chai Kanchan <cdkanchan@gmail.com>
2022-12-03 17:51:32 +0000

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/weichsel/ZIPFoundation/", .upToNextMajor(from: "0.9.0")),
-        .package(url: "https://github.com/ckanchan/CDKSwiftOracc", .branch("master"))
+        .package(url: "https://github.com/ckanchan/CDKSwiftOracc", .branch("main"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
